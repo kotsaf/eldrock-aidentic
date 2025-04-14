c34db930ae612a38d22a058763b01823d8ac5c99
safqw <safqwer.136@gmail.com>
2025-04-15 02:42:11 +0300

    Доработка проекта: - Добавлены анимации для кнопок и заголовка - Добавлены фоновые изображения для секций - Добавлена форма для связи - Добавлены анимации для формы - Добавлены анимации для кнопок - Добавлены анимации для заголовка

--- eldrock/eldrock/settings.py
+++ eldrock/eldrock/settings.py
@@ -1,21 +1,17 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
---
+++ eldrock/main/static/main/js/scripts.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('aside nav button');
+const title = document.querySelector('.section1 h1');
+
+buttons.forEach((button, index) => {
+    setTimeout(() => {
+        anime({
+            targets: button,
+            duration: 100,
+            translateX: '0%',
+            opacity: 1,
+            easing: 'easeOutExpo',
+            complete: function() {
+                
+                if (index === buttons.length - 1) {
+                    setTimeout(() => {
+                        anime({
+                            targets: title,
+                            duration: 1500,
+                            translateY: '0',
+                            opacity: 1,
+                            easing: 'easeOutExpo'
+                        });
+                    }, 300);
+                }
+            }
+        });
+    }, 500 * (index + 1)); // Задержка для каждой кнопки
+});
