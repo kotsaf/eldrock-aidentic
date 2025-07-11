16bbbbaa8572fe47fb56c6e344384beb6b0987f5
Safar <safqwer.136@gmail.com>
2025-07-12 01:46:58 +0300

    Update manage.py

--- eldrock/manage.py
+++ eldrock/manage.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eldrock.settings')
     try:
         from django.core.management import execute_from_command_line
